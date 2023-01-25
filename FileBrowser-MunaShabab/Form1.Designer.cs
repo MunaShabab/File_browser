9dae249dda8cc0d50970b60369497ec81cca341e
MunaShabab <80338619+MunaShabab@users.noreply.github.com>
2023-01-25 13:02:05 -0500

    Add files via upload

---
+++ FileBrowser-MunaShabab/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace FileReader_MunaShabab
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileNameDesLabel = new System.Windows.Forms.Label();
+            this.fileNameTextBox = new System.Windows.Forms.TextBox();
+            this.readButton = new System.Windows.Forms.Button();
+            this.displayListBox = new System.Windows.Forms.ListBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // fileNameDesLabel
+            // 
+            this.fileNameDesLabel.AutoSize = true;
+            this.fileNameDesLabel.Location = new System.Drawing.Point(95, 19);
+            this.fileNameDesLabel.Name = "fileNameDesLabel";
+            this.fileNameDesLabel.Size = new System.Drawing.Size(202, 13);
+            this.fileNameDesLabel.TabIndex = 0;
+            this.fileNameDesLabel.Text = "Please Enter A File Path And a File Name";
+            // 
+            // fileNameTextBox
+            // 
+            this.fileNameTextBox.Location = new System.Drawing.Point(98, 49);
+            this.fileNameTextBox.Name = "fileNameTextBox";
+            this.fileNameTextBox.Size = new System.Drawing.Size(199, 20);
+            this.fileNameTextBox.TabIndex = 1;
+            // 
+            // readButton
+            // 
+            this.readButton.Location = new System.Drawing.Point(127, 88);
+            this.readButton.Name = "readButton";
+            this.readButton.Size = new System.Drawing.Size(146, 23);
+            this.readButton.TabIndex = 2;
+            this.readButton.Text = "Read The File";
+            this.readButton.UseVisualStyleBackColor = true;
+            this.readButton.Click += new System.EventHandler(this.readButton_Click);
+            // 
+            // displayListBox
+            // 
+            this.displayListBox.FormattingEnabled = true;
+            this.displayListBox.Location = new System.Drawing.Point(43, 144);
+            this.displayListBox.Name = "displayListBox";
+            this.displayListBox.Size = new System.Drawing.Size(318, 134);
+            this.displayListBox.TabIndex = 3;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // browseButton
+            // 
+            this.browseButton.Location = new System.Drawing.Point(303, 46);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(75, 23);
+            this.browseButton.TabIndex = 4;
+            this.browseButton.Text = "Browse...";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(408, 314);
+            this.Controls.Add(this.browseButton);
+            this.Controls.Add(this.displayListBox);
+            this.Controls.Add(this.readButton);
+            this.Controls.Add(this.fileNameTextBox);
+            this.Controls.Add(this.fileNameDesLabel);
+            this.Name = "Form1";
+            this.Text = "File Reader Demo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label fileNameDesLabel;
+        private System.Windows.Forms.TextBox fileNameTextBox;
+        private System.Windows.Forms.Button readButton;
+        private System.Windows.Forms.ListBox displayListBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button browseButton;
+    }
+}
+
---
+++ FileBrowser-MunaShabab/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FileReader_MunaShabab
+{
+    //author: Muna Shabab
+    //ID:624191
+    //Date:12-1-19
+    //Goal:to demonstrate file reading of user specified file
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void readButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //declare a streamReader object
+                StreamReader inputFile;
+                inputFile = File.OpenText(fileNameTextBox.Text);
+
+                //clear the content of the listBox
+                displayListBox.Items.Clear();
+
+                //while the file is not done 
+                while(!inputFile.EndOfStream)
+                {
+                    //read a line
+                    string line;
+                    line=inputFile.ReadLine();
+
+                    //and display it in the listBox
+                    displayListBox.Items.Add(line);
+                }
+
+                //close the file
+                inputFile.Close();
+            }
+          
+            //show the error message
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+            {
+                //the code below opens the file browser dialog
+                //and tracks if the user clicked on Open or Cancel when looking at Files
+                //if they clicked on Open the file path will be placed the fileNameTextBox on the form
+                //if they click on Cancel the fileNameTextBox will be set to blank
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    fileNameTextBox.Text = openFileDialog1.FileName;
+                }
+                else
+                {
+                    //do nothing, user canceled out of selecting a file
+                   fileNameTextBox.Text = "";
+                }
+            }
+        }
+    }
+}
